047264afd4bdbc902aff783b281b6351d38eb667
sunita0987 <sunita0987546@gmail.com>
2025-07-17 12:44:55 +0530

    first commit

--- src/components/PizzaGallery.jsx
+++ src/components/PizzaGallery.jsx
@@ -1,39 +1,13 @@
 import React from "react";
 import { GALLERY_IMG } from "../utils/helper";
 
-// const Gallery = () => {
-//   return (
-//     <div className="max-w-[1320px] mx-auto px-3 mt-[77px] text-center">
-//       <h3 className="text-orange-500 font-semibold mb-2">Our Gallery</h3>
-//       <h2 className="text-3xl md:text-4xl font-bold mb-10">
-//         A Glimpse Into Our Pizza World
-//       </h2>
-
-//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 auto-rows-[200px] gap-3 md:gap-6">
-//         {GALLERY_IMG.map((img, index) => (
-//           <div
-//             key={index}
-//             className={`overflow-hidden rounded-2xl ${img.colSpan} ${img.rowSpan}`}
-//           >
-//             <img
-//               src={img.image}
-//               alt="gallery"
-//               className=" object-cover transition duration-300"
-//             />
-//           </div>
-//         ))}
-//       </div>
-
-//       <button className="mt-10 bg-orange-500 hover:bg-orange-600 text-white font-semibold px-6 py-2 rounded-full transition">
-//         View More
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Gallery;
-const PizzaGallery = () => {
+const Gallery = () => {
   return (
+    <div className="max-w-[1320px] mx-auto px-3 mt-[77px] text-center">
+      <h3 className="text-[#EC6112] font-semibold mb-2">Our Gallery</h3>
+      <h2 className="text-3xl md:text-4xl font-normal text-center ml-100 w-[400px] mb-10">
+        A Glimpse Into Our Pizza World
+      </h2>
     <div className="max-w-[1140px] px-3 mx-auto">
       <div className="grid lg:grid-cols-5 gap-6">
         {GALLERY_IMG.map((item, index) => (
@@ -49,8 +23,12 @@ const PizzaGallery = () => {
           </div>
         ))}
       </div>
+       <button className="mt-10 bg-gradient-to-r from-[#EC6112] to-[#FF902E] text-[#fffff] font-semibold px-6 py-2  mb-10 rounded-full transition cursor-pointer">
+        View More
+      </button>
+    </div>
     </div>
   );
 };
 
-export default PizzaGallery;
+export default Gallery;
